6ce2849dedcd4d75a6cb6131e9c636823d795c09
kir0513 <kir0513@yandex.ru>
2023-05-08 12:39:59 +0300

    Не создает нового пользователя, не обновляет

--- src/main/java/web/service/UserServiceImp.java
+++ src/main/java/web/service/UserServiceImp.java
@@ -2,11 +2,13 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDao;
 import web.model.User;
 
 import java.util.List;
 @Repository
+@Transactional
 public class UserServiceImp implements UserService{
     private final UserDao userDao;
     @Autowired
