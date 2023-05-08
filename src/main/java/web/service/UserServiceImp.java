fa0892dbfd1c6e5a010a5ad194a4b50b68e5d7be
kir0513 <kir0513@yandex.ru>
2023-05-08 10:50:34 +0300

    Соединился с бд. Вышел на главную страницу. Перешел по ссылке добавит пользователя. При добавлении выскакивает ошибка

--- src/main/java/web/service/UserServiceImp.java
+++ src/main/java/web/service/UserServiceImp.java
@@ -1,5 +1,6 @@
 package web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import web.dao.UserDao;
 import web.model.User;
@@ -8,6 +9,7 @@ import java.util.List;
 @Repository
 public class UserServiceImp implements UserService{
     private final UserDao userDao;
+    @Autowired
     public UserServiceImp(UserDao userDao) {
         this.userDao = userDao;
     }
